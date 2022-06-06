1b7d0a7533fe87410fa5a6d44b8b8895cbd3b7b3
klutsai <lutsai.k@gmail.com>
2022-06-06 15:38:48 +0200

    lab 5

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,9 +1,17 @@
 from imageai.Prediction import ImagePrediction
 from imageai.Detection import ObjectDetection
+from imageai.Detection import VideoObjectDetection
 import os
+from zipfile import ZipFile
+import cv2
+import numpy as np
 
-model1 = 'resnet50_weights_tf_dim_ordering_tf_kernels.h5'
-model2 = 'resnet50_coco_best_v2.0.1.h5'
+model1 = 'resnet50_imagenet_tf.2.0.h5'
+model2 = 'resnet50_coco_best_v2.1.0.h5'
+model3 = 'yolo.h5'
+image = "cat.jpg"
+video = "video.MP4"
+fruits = "archive.zip"
 
 files = ["cherries7.jpg",
          "kachi.jpg",
@@ -18,21 +26,43 @@ prediction.setModelTypeAsResNet()
 prediction.setModelPath(os.path.join(execution_path, model1))
 prediction.loadModel()
 
-for file in files:
-    print(file)
-    predictions, percentage_probabilities = prediction.predictImage(file, result_count = 5)
-    
-    for i in range(len(predictions)):
-        print(predictions[i], ":", percentage_probabilities[i])
-        
-detector = ObjectDetection()
-detector.setModelTypeAsRetinaNet()
-detector.setModelPath(os.path.join(os.getcwd(), model2))
+img_detector = ObjectDetection()
+img_detector.setModelTypeAsRetinaNet()
+img_detector.setModelPath(os.path.join(os.getcwd(), model2))
+img_detector.loadModel()
 
-detector.loadModel()
-detections = detector.detectObjectsFromImage(
-    input_image=os.path.join(execution_path, "cat.jpg"), 
-    put_image_path=os.path.join(execution_path, "cat_new.jpg"))
+vid_detector = VideoObjectDetection()
+vid_detector.setModelTypeAsYOLOv3()
+vid_detector.setModelPath( os.path.join(execution_path , model3))
+vid_detector.loadModel()
+
+zip = ZipFile(fruits, "r")
+for name in files:
+    for file in zip.namelist():
+        if name in file:
+            data = zip.read(file)
+            img = cv2.imdecode(np.frombuffer(data, np.uint8), 1)
+            img = cv2.resize(img, None, fx=0.25, fy=0.25, interpolation=cv2.INTER_AREA)
+            cv2.imshow(name, img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+            zip.extract(file)
+            extracted = "fruits-360_dataset/fruits-360/test-multiple_fruits/"+name
+            print(extracted)
+            predictions, percentage_probabilities = prediction.classifyImage(extracted, result_count = 5)
+
+            for i in range(len(predictions)):
+                print(predictions[i], ":", percentage_probabilities[i])
+
+detections = img_detector.detectObjectsFromImage(
+    input_image=os.path.join(execution_path, image),
+    output_image_path=os.path.join(execution_path, "image_detected.jpg"))
 
 for eachObject in detections:
     print(eachObject["name"], ":", eachObject["percentage_probability"])
+
+video_path = vid_detector.detectObjectsFromVideo(input_file_path=os.path.join(execution_path, video),
+                                             output_file_path=os.path.join(execution_path, "video_detected"),
+                                             frames_per_second=10, log_progress=True)
+print(video_path)
